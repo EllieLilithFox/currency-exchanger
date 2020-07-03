efc419afad091d00dd3432a0e134b5951a20ab91
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-03 10:24:17 -0700

    add currency dropdown and api call function

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,9 +1,16 @@
 export class Currency {
-  constructor(usd, eur, krw, nzd, cad) {
+  constructor(usd, jpy, eur, krw, nzd, cad) {
     this.usd = usd;
+    this.jpy = usd * jpy;
     this.eur = usd * eur;
     this.krw = usd * krw;
     this.nzd = usd * nzd;
     this.cad = usd * cad;
   }
+}
+
+export const getCurrencyRates = async () => {
+  const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+  const data = await response.json();
+  return data;
 }
