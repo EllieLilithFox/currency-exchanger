265870c9dc9f83b7dba26f628c8bcbed5dd4770b
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-03 14:13:24 -0700

    flesh out catch statement

--- src/main.js
+++ src/main.js
@@ -2,13 +2,27 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
-import { getCurrencyRates } from "./currency-service";
+import { Currency, getCurrencyRates } from "./currency-service";
 
 $(document).ready(() => {
   $("#input").click(() => {
+    let currency;
     event.preventDefault();
-    getCurrencyRates().then(data => {
-      console.log(data);
+    getCurrencyRates().then((response) => {
+      if (!response.ok) {
+        throw new Error(response.staus);
+      }
+      let data = response.json();
+      currency = new Currency(
+        parseFloat($("#usd").val()),
+        data.conversion_rates.JPY,
+        data.conversion_rates.EUR,
+        data.conversion_rates.KRW,
+        data.conversion_rates.NZD,
+        data.conversion_rates.CAD
+      );
+    }).catch((error) => {
+      $("#output").text(`The request failed with a error code ${error}`);
     });
   });
 });
